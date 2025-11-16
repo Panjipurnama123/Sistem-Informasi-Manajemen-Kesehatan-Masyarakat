9093802848607b20568f2bb9fb5d3118b84958ea
Panjipurnama123 <uyupur0@gmail.com>
2025-11-16 09:18:35 +0700

    Add header.php for website structure

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Informasi Manajemen Kesehatan Masyarakat</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="about.php">Tentang</a></li>
+                <li><a href="services.php">Layanan</a></li>
+                <li><a href="contact.php">Kontak</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
